30fdf0c6a4d4327db1834fcf1824c184d7010092
Germano Nascimento <nascimento.germano@gmail.com>
2024-12-16 06:23:10 -0300

    adicionando exercicios iniciais

---
+++ main_02.py
@@ -0,0 +1,22 @@
+# 1
+valor1 = int(input('Digite o primeiro numero: '))
+valor2 = int(input('Digite o segundo numero: '))
+soma = valor1 + valor2
+print(soma)
+
+# 2
+valor3 = int(input('Digite um numero inteiro: '))
+resto = valor3 % 5
+print(resto)
+
+# 3
+valor4 = int(input('Digite o primeiro numero: '))
+valor5 = int(input('Digite o segundo numero: '))
+mult = valor4 * valor5
+print(mult)
+
+# 4
+valor6 = int(input('Digite o primeiro numero: '))
+valor7 = int(input('Digite o segundo numero: '))
+div_int = valor6 // valor7
+print(div_int)
