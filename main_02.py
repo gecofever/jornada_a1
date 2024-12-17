3d41c1f6a6f3a4ce984b413fb8fb30b436437aae
Germano Nascimento <nascimento.germano@gmail.com>
2024-12-17 05:48:44 -0300

    modificacoes na main_02.py e inclusão da main_03.py

--- main_02.py
+++ main_02.py
@@ -8,6 +8,7 @@ print(soma)
 valor3 = int(input('Digite um numero inteiro: '))
 resto = valor3 % 5
 print(resto)
+print()
 
 # 3
 valor4 = int(input('Digite o primeiro numero: '))
@@ -20,3 +21,15 @@ valor6 = int(input('Digite o primeiro numero: '))
 valor7 = int(input('Digite o segundo numero: '))
 div_int = valor6 // valor7
 print(div_int)
+print()
+
+# 5
+valor8 = int(input('Digite um numero: '))
+quadrado = valor8 ** 2
+print(quadrado)
+
+# 6
+valor9 = float(input('Digite o primeiro numero: '))
+valor10 = float(input('Digite o segundo numero: '))
+soma2 = valor9 + valor10
+print(soma2)
---
+++ main_03.py
@@ -0,0 +1,17 @@
+# 7
+valor1 = float(input('Digite o primeiro numero: '))
+valor2 = float(input('Digite o segundo numero: '))
+media = (valor1 + valor2) / 2
+print(media)
+
+# 8
+base = float(input('Digite uma base: '))
+expoente = float(input('Digite um expoente: '))
+potencia = base ** expoente
+print(f'{potencia:.2f}')
+
+# 9
+
+celsius = float(input('Digite uma temperatura em Celsius: '))
+farenheigt = (celsius * 1.8) + 32
+print(f'{farenheigt:.2f}')
